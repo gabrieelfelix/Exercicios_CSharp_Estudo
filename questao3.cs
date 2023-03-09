890537e3ca575195c41675b6ea57802140e689a8
Gabriel Felix <102039571+gabrieelfelix@users.noreply.github.com>
2023-03-09 12:27:03 -0400

    Add files via upload

---
+++ questao3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual sua idade? \n");
+            Console.WriteLine("Anos: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Meses: ");
+            int meses = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dias: ");
+            int dias = Convert.ToInt32(Console.ReadLine());
+            int resultado = (idade * 365) + (meses * 30) + dias;
+            Console.WriteLine(resultado + " Dias de vida!");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ questao6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float custo_fabrica, resultado;
+
+            Console.WriteLine("Digite o Custo de Fábrica: ");
+            custo_fabrica = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+
+            resultado = custo_fabrica + ((custo_fabrica * 28) / 100) + ((custo_fabrica * 45) / 100);
+            Console.WriteLine("Valor Final: R$" + resultado);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
